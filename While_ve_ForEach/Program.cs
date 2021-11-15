bdbaa58f080ff00a79f3ed52e5eafe1ae0534dbf
Fatih Ceylan <86945451+MFCRepositories@users.noreply.github.com>
2021-11-15 17:18:56 +0300

    While ve ForEach Döngüleri

---
+++ While_ve_ForEach/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace While_ve_ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool durum = true;
+            //while (durum)
+            //{
+            //    Console.WriteLine("Hello Yazılım 131");
+            //    durum = false;
+            //}
+
+            //int i = 0;
+            //while (i<10)
+            //{
+            //    Console.WriteLine("Hello");
+            //    i++;
+            //}
+
+            //bool durum = true;
+            //while (durum)
+            //{
+            //    Console.WriteLine("1.Tamam 2.Devam");
+            //    char secim = Console.ReadKey(true).KeyChar;
+            //    if (secim == '1')
+            //        durum = false;
+            //}
+
+            //char secim2 = '2';
+            //while (secim2=='2')
+            //{
+            //    Console.WriteLine("1.Tamam 2.Devam");
+            //    secim2 = Console.ReadKey(true).KeyChar;
+            //}
+
+            //int i = 0;
+            //while (i<10)
+            //{
+            //    int j = 0;
+            //    while (j<10)
+            //    {
+            //        Console.Write("♥");
+            //        j++;
+            //    }
+            //    Console.WriteLine();
+            //    i++;
+            //}
+
+        }
+    }
+}
