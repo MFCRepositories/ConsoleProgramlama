3dfb359eec000e47154e09ec62f77d758bc84a26
Fatih Ceylan <86945451+MFCRepositories@users.noreply.github.com>
2021-11-15 16:40:39 +0300

    If_Else_Ternary

---
+++ If-Else-Ternary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace If_Else_Ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool condition = true;
+            int gel = 5;
+
+            if (condition)
+            {
+                Console.WriteLine("Değişken: true");
+            }
+            else
+            {
+                Console.WriteLine("Değişken: false");
+            }
+
+            string sonuc= gel==5 ?  "true" : "false";
+        }
+    }
+}
