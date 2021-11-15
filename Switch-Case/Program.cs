6cd9b6817e5385fd1a4501401f8ef28a482a5a45
Fatih Ceylan <86945451+MFCRepositories@users.noreply.github.com>
2021-11-15 16:53:35 +0300

    Switch-Case

---
+++ Switch-Case/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Secimlerinizi gerçekleştiriniz...");
+            string degisken = Console.ReadLine();
+
+            switch (degisken)
+            {
+                case "A":
+                    Console.WriteLine("A ile başlayan kız isimleri");
+                    break;
+                case "B":
+                    Console.WriteLine("B ile başlayan kız isimleri");
+                    break;
+                case "C":
+                    Console.WriteLine("C ile başlayan kız isimleri");
+                    break;
+                default:
+                    Console.WriteLine("Seçim yapmadınız");
+                    break;
+            }
+        }
+    }
+}
