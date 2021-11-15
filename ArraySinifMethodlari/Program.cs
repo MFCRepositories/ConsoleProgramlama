33cead59f36407fdc618ebb2b93aa7ef30cf375c
Fatih Ceylan <86945451+MFCRepositories@users.noreply.github.com>
2021-11-15 17:30:57 +0300

    Array Sınıfı Methodları

---
+++ ArraySinifMethodlari/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ArraySinifMethodlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] personeller = new string[0];
+
+            bool durum = true;
+            while (durum)
+            {
+                Console.WriteLine("1.Personel Ekle\n----\n2.Personelleri Görüntüle\n----\n3.Çıkış");
+                char secim = Console.ReadKey(true).KeyChar;
+
+                switch (secim)
+                {
+                    case '1':
+                        Array.Resize(ref personeller, personeller.Length + 1);
+                        Console.Write("Personel adı: ");
+                        string ad = Console.ReadLine();
+                        Console.Write("Personel soyadı: ");
+                        string soyad = Console.ReadLine();
+                        personeller[personeller.Length - 1] = ad + " " + soyad;
+                        break;
+                    case '2':
+                        Console.WriteLine("----\nPERSONEL LİSTESİ\n----");
+                        for (int i = 0; i < personeller.Length; i++)
+                        {
+                            Console.WriteLine((i + 1) + "." + personeller[i]);
+                        }
+                        Console.WriteLine("----");
+                        break;
+                    case '3':
+                        durum = false;
+                        break;
+                    default:
+                        Console.WriteLine("Lütfen geçerli bir seçim yapınız");
+                        break;
+                }
+            }
+        }
+    }
+}
