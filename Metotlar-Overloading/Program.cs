b0ddc0b9593c5114d924180bdc1a21a360bb2244
Fatih Ceylan <86945451+MFCRepositories@users.noreply.github.com>
2021-11-15 22:29:07 +0300

    Metotlar - Overloading

---
+++ Metotlar-Overloading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Metotlar_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Method overload aynı isimde farklı parametrelere sahip birden fazla method tanımlanmasıdır.
+
+            SayHello();
+            SayHello("Hello World");
+            SayHello("İdil", "Hello");
+
+            Console.ReadKey();
+        }
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void SayHello(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        static void SayHello(string ad, string msg)
+        {
+            Console.WriteLine(msg + " " + ad);
+        }
+    }
+}
