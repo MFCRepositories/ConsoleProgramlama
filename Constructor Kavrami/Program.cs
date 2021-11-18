9cfbaa878a7aa08d15ddf9e104ac7d4533bfe012
Fatih Ceylan <86945451+MFCRepositories@users.noreply.github.com>
2021-11-18 15:53:02 +0300

    Constructor_Kavrami

---
+++ Constructor Kavrami/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Constructor_Kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir sınıftan bir nesne oluşturulduğunda biz tanımlamasak bile arka planda çalışan varsayılan yapıcı metotlara kurucu yada constructor denir. Sınıf nesnesi ilk oluşturulduğunda yapılmasını istediğimiz işleri kurucu metotlar içerisinde yaparız.
+         
+            // Kurucu eetot tanımlarken dikkat edilmesi gereken noktalar ise şu şekildedir:
+            // Yapıcı metotların isimleri sınıf isimleri ile aynı olmak zorundadır.
+            // Public olarak bildirilmeleri gerekir.
+            // Geri dönüş değerleri yoktur.
+        }
+    }
+}
